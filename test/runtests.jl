ff3be83b2f459bedab8db2a73e16d5925d22efc1
Christensen <zchristensen@nsc-foxe33.urmc-sh.rochester.edu>
2019-11-21 15:32:09 -0500

    Added basic addition test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,20 @@
 using AbstractIndices, Test, Statistics
+
 import Base: OneTo
 
 #include("uniqueness_tests.jl")
 include("matmul_tests.jl")
+
+@testset "Addition" begin
+    a = IndicesArray(ones(3))
+    @test +(a) == ones(3)
+
+    @test +(a, a) == 2ones(3)
+
+    @test +(a, a, a) == 3ones(3)
+end
+
+
 
 #=
 include("abstractindex.jl")
